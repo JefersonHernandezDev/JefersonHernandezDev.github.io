cfce591dbf19c5f96e0047f252da6d6d13a20082
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-05-06 10:57:35 -0500

    remove unnesesary params

--- svg_generator_v1/index.js
+++ svg_generator_v1/index.js
@@ -10,7 +10,6 @@ function generateLabels({
   const xOffset = totalWidth / availableIntervals; // 1024 / (100 / 5)
 
   for (let index = 0; index <= availableIntervals; index++) {
-    console.log({ index, intervalValue });
     labels.push(
       `<text x="0" y="15" fill="#666565" transform="matrix(1 0 0 1 ${
         index * xOffset
@@ -22,8 +21,7 @@ function generateLabels({
 }
 
 function generateSvg({
-  totalWidth,
-  solidwidth,
+  totalWidth = 1000,
   basicColor = "#62A33D",
   cutof = 8,
   value,
@@ -31,7 +29,6 @@ function generateSvg({
   intervalValue,
   cutofValue = 0,
 }) {
-  // const gradientOffset = solidwidth + cutof;
   const barHeight = 8;
   const cutofHeight = barHeight * 2.5;
 
@@ -45,100 +42,86 @@ function generateSvg({
   const cutofOffset = xOffsetByInterval * (cutofValue / intervalValue);
   const gradientOffset = cutofOffset + cutof;
 
-  document.querySelector(
-    "#midcontentadcontainer"
-  ).innerHTML = `<?xml version="1.0" encoding="utf-8"?> 
-        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-        <svg version="1.1" id="Fullbar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-             width="1024px" height="100px" viewBox="0 -32.669 1024 82.331" enable-background="new 0 -32.669 1024 82.331"
-             xml:space="preserve">
-        
-        <linearGradient id="GradientBar_1_" gradientUnits="userSpaceOnUse" x1=${gradientOffset} y1="7.6177" x2=${totalWidth} y2="7.6177">
-                  <stop offset="0.0" style="stop-color:'#62A33D';stop-opacity:1.00" />
-                  <stop offset="0.0" style="stop-color:#62A33D;stop-opacity:1.00" />
-        
-                  <stop offset="0.4" style="stop-color:rgb(230,247,12);stop-opacity:1.00" />
-                  <stop offset="0.6" style="stop-color:rgb(230,247,12);stop-opacity:1.00" />
-        
-                  <stop offset="1.0" style="stop-color:rgb(197,6,22);stop-opacity:1.00" />
-                  <stop offset="1.0" style="stop-color:rgb(197,6,22);stop-opacity:1.00" />
-        </linearGradient>
-        <rect x="0" width=${solidwidth} height=${barHeight} fill='${basicColor}'/>
-        <rect x=${cutofOffset}  y="-5"  width=${cutof} rx="3" ry="3" height=${cutofHeight} fill="#6eb5ff" />
-        <rect x=${gradientOffset} width=${totalWidth} height=${barHeight} fill="url(#GradientBar_1_)" />
-        <rect x=${dotOffset} y="-5" width=${dotSize} rx="50" ry="50" height=${dotSize} fill="#62A33D"   stroke="#f5f5f5" stroke-width="4"  />
-        <text y=${50} fill="#62A33D" x=${dotOffset} font-size="18">${value}</text>
-        <g>
-          ${generateLabels({
-            totalWidth,
-            labelsYPos,
-            limiteSuperior,
-            intervalValue,
-          })}
-        </g>
-        </svg>`;
+  let svg = `<?xml version="1.0" encoding="utf-8"?> 
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  <svg version="1.1" id="Fullbar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       width="1024px" height="100px" viewBox="0 -32.669 1024 82.331" enable-background="new 0 -32.669 1024 82.331"
+       xml:space="preserve">
+  
+  <linearGradient id="GradientBar_1_" gradientUnits="userSpaceOnUse" x1="${gradientOffset}" y1="7.6177" x2="${totalWidth}" y2="7.6177">
+            <stop offset="0.0" style="stop-color:'#62A33D';stop-opacity:1.00" />
+            <stop offset="0.0" style="stop-color:#62A33D;stop-opacity:1.00" />
+  
+            <stop offset="0.4" style="stop-color:rgb(230,247,12);stop-opacity:1.00" />
+            <stop offset="0.6" style="stop-color:rgb(230,247,12);stop-opacity:1.00" />
+  
+            <stop offset="1.0" style="stop-color:rgb(197,6,22);stop-opacity:1.00" />
+            <stop offset="1.0" style="stop-color:rgb(197,6,22);stop-opacity:1.00" />
+  </linearGradient>
+  <rect x="0" width="${cutofOffset}" height="${barHeight}" fill="${basicColor}"/>
+  <rect x="${cutofOffset}"  y="-5"  width="${cutof}" rx="3" ry="3" height="${cutofHeight}" fill="#f5f5f5" />
+  <rect x="${gradientOffset}" width="${totalWidth}" height="${barHeight}" fill="url(#GradientBar_1_)" />
+  <rect x="${dotOffset}" y="-5" width="${dotSize}" rx="50" ry="50" height="${dotSize}" fill="#62A33D"   stroke="#f5f5f5" stroke-width="4"  />
+  <text y="${50}" fill="#62A33D" x="${dotOffset}" font-size="18">${value}</text>
+  <g>
+    ${generateLabels({
+      totalWidth,
+      labelsYPos,
+      limiteSuperior,
+      intervalValue,
+    })}
+  </g>
+  </svg>`;
+
+  document.querySelector("#midcontentadcontainer").innerHTML = svg;
+  document.querySelector("#svg_string").innerText = svg;
 }
 
-function generateBasicSvg() {
+document.getElementById("btnGenerate").onclick = () => {
   generateSvg({
-    totalWidth: 1000,
-    solidwidth: 700,
-    value: 15,
-    limiteSuperior: 600,
-    intervalValue: 30,
+    value: document.querySelector("#inputValue").value,
+    limiteSuperior: document.querySelector("#inputLimiteSuperior").value,
+    intervalValue: document.querySelector("#inputIntervalo").value,
+    cutofValue: document.querySelector("#inputCutofValue").value,
   });
-}
-
-function generateTwoSvg() {
-  generateSvg({
-    totalWidth: 1000,
-    solidwidth: 700,
-    value: 10,
-    limiteSuperior: 200,
-    intervalValue: 40,
-  });
-}
-
-function generateThreeSvg() {
-  generateSvg({
-    totalWidth: 1000,
-    solidwidth: 700,
-    value: 70,
-    limiteSuperior: 300,
-    intervalValue: 10,
-  });
-}
+};
 
 document.getElementById("btnBasic").onclick = () => {
-  generateBasicSvg();
+  generateSvg({
+    totalWidth: 1000,
+    value: 30,
+    limiteSuperior: 50,
+    intervalValue: 5,
+    cutofValue: 45,
+  });
 };
 
 document.getElementById("btnExampleOne").onclick = () => {
-  generateTwoSvg();
+  generateSvg({
+    totalWidth: 1000,
+    value: 30,
+    limiteSuperior: 50,
+    intervalValue: 5,
+    cutofValue: 10,
+  });
 };
 
 document.getElementById("btnExampleTwo").onclick = () => {
-  generateThreeSvg();
+  generateSvg({
+    totalWidth: 1000,
+    value: 30,
+    limiteSuperior: 100,
+    intervalValue: 15,
+    cutofValue: 45,
+  });
 };
 
 document.getElementById("btnExampleThree").onclick = () => {
   generateSvg({
     totalWidth: 1000,
-    solidwidth: 300,
     value: 150,
     limiteSuperior: 200,
     intervalValue: 10,
     cutofValue: 20,
-  });
-};
-
-document.getElementById("btnGenerate").onclick = () => {
-  generateSvg({
-    totalWidth: 1000,
-    solidwidth: 300,
-    value: document.querySelector("#inputValue").value,
-    limiteSuperior: document.querySelector("#inputLimiteSuperior").value,
-    intervalValue: document.querySelector("#inputIntervalo").value,
-    cutofValue: document.querySelector("#inputCutofValue").value,
   });
 };
